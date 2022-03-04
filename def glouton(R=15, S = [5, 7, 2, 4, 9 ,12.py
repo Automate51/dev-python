dcad074bf429442fb25112aa03955f933dca2a53
Automate51 <96928520+Automate51@users.noreply.github.com>
2022-03-04 15:38:35 +0100

    Update def glouton(R=15, S = [5, 7, 2, 4, 9 ,12.py

--- def glouton(R=15, S = [5, 7, 2, 4, 9 ,12.py
+++ def glouton(R=15, S = [5, 7, 2, 4, 9 ,12.py
@@ -5,7 +5,7 @@ def glouton(R=15, S = [5, 14, 2, 4, 10 , 14, 5, 6, 9, 8, 5, 12, 14, 15, 13, 14])
     while di != nb_stations:
 
         if R < d:
-            print("Vous tombez en panne cheh !!!")
+            print("Vous tombez en panne !!!")
             
             break
         else:
