0995d423d2ab53df99531c8158975cc0d387bdaa
Automate51 <96928520+Automate51@users.noreply.github.com>
2022-03-04 15:33:04 +0100

    Add files via upload
    EXERCICE: Algorithme glouton.
    Vous souhaitez vous rendre de Rennes à Toulouse en voiture.
    
    votre réservoir vous permet de rouler R kilomètres.
    
    Vous connaissez la liste S des pompes à essences disponibles sur votre route telle que S = [d1, d2, d3, d4 ... dk] où dk symbolisant l'arrivée. On a donc:
     - S[0] est à d1 kilomètre du départ
     - S[2] est à d2 kilomètre de S[0]
     - Etc...
    
    On veut faire le moins d'arrêt possible et avec R = 15 et S = [5, 7, 2, 4, 9, 12] implémentez l'algorithme glouton en écrivant un fonction d'argument S, R ou S est la liste précédente et R la capacité du réservoir en kilomètre. la fonction devra renvoyer la liste des numéros des pompes utilisées; 2, 4, 5.

---
+++ def glouton(R=15, S = [5, 7, 2, 4, 9 ,12.py
@@ -0,0 +1,26 @@
+def glouton(R=15, S = [5, 14, 2, 4, 10 , 14, 5, 6, 9, 8, 5, 12, 14, 15, 13, 14]):
+    di = 0
+    d = S[di]
+    nb_stations = len(S) - 1
+    while di != nb_stations:
+
+        if R < d:
+            print("Vous tombez en panne cheh !!!")
+            
+            break
+        else:
+            while  d <= R:
+                 di += 1
+                 d += S[di]
+                 arret = di-1
+        
+            
+        print (arret)
+        d = S[di]
+    print ("Arrêt final: arrêt numéro ",di)
+
+    
+    
+    
+glouton()
+    
